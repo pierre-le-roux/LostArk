8f3d4b40b7159e2b0026eaacda69319f6598e940
Pierre Le Roux <leroux.pierre@gmail.com>
2022-04-21 16:50:45 +0200

    feat: Computer vision with pyautogui

--- video.py
+++ video.py
@@ -1,15 +1,49 @@
 import cv2 as cv
 import numpy as np
-import pyautogui
+from time import time
+import win32gui, win32ui, win32con
 
+
+def window_capture():
+
+    # size of my monitor
+    w = 1920
+    h = 1080
+    bmpfilenamename = "debug.bmp"
+
+    #hwnd = win32gui.FindWindow(None, windowname)
+    hwnd = None
+
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj=win32ui.CreateDCFromHandle(wDC)
+    cDC=dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
+
+    dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
+
+    # Free Resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+
+
+
+loop_time = time()
 while(True):
 
-    screenshot = pyautogui.screenshot()
-
+    screenshot = window_capture()
     screenshot = np.array(screenshot)
     screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
 
     cv.imshow('Computer Vision', screenshot)
+
+    # measure fps
+    print('FPS {}'.format(1 / (time() - loop_time)))
+    loop_time = time()
 
     # press 'q' with the output window focused to exit.
     # waits 1 ms every loop to process key presses
