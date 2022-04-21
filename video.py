9c1001542025ae1dc866b1be8514439bb80750e7
Pierre Le Roux <leroux.pierre@gmail.com>
2022-04-21 17:01:47 +0200

    feat: Add window_capture and list_window_names functions

--- video.py
+++ video.py
@@ -1,3 +1,4 @@
+import imghdr
 import cv2 as cv
 import numpy as np
 from time import time
@@ -5,14 +6,16 @@ import win32gui, win32ui, win32con
 
 
 def window_capture():
+    '''
+    Capture a screenshot, turn it into a format opencv can read and return it
+    '''
 
     # size of my monitor
     w = 1920
     h = 1080
     bmpfilenamename = "debug.bmp"
 
-    #hwnd = win32gui.FindWindow(None, windowname)
-    hwnd = None
+    hwnd = win32gui.FindWindow(None, windowname)
 
     wDC = win32gui.GetWindowDC(hwnd)
     dcObj=win32ui.CreateDCFromHandle(wDC)
@@ -21,8 +24,12 @@ def window_capture():
     dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
     cDC.SelectObject(dataBitMap)
     cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
-
-    dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
+    
+    # save the screenshot
+    #dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
+    signedIntsArray = dataBitMap.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (h, w, 4)
 
     # Free Resources
     dcObj.DeleteDC()
@@ -30,14 +37,17 @@ def window_capture():
     win32gui.ReleaseDC(hwnd, wDC)
     win32gui.DeleteObject(dataBitMap.GetHandle())
 
+    img = img[...,:3]
+
+    img = np.ascontiguousarray(img)
+
+    return img
 
 
 loop_time = time()
 while(True):
 
     screenshot = window_capture()
-    screenshot = np.array(screenshot)
-    screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
 
     cv.imshow('Computer Vision', screenshot)
 
@@ -52,3 +62,13 @@ while(True):
         break
 
 print('Done.')
+
+
+def list_window_names():
+    def winEnumHandler(hwnd, ctx):
+        if win32gui.IsWindowVisible(hwnd):
+            print(hex(hwnd), win32gui.GetWindowText(hwnd))
+    win32gui.EnumWindows(winEnumHandler, None)
+
+# call it here:
+# list_window_names()
