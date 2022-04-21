af3e34cbd4c6c11abca4f4ac2bf987be94d52f8a
Pierre Le Roux <leroux.pierre@gmail.com>
2022-04-21 17:18:34 +0200

    feat: list_window_names moved to functions.py

---
+++ functions.py
@@ -0,0 +1,7 @@
+import win32gui, win32ui, win32con
+
+def list_window_names():
+    def winEnumHandler(hwnd, ctx):
+        if win32gui.IsWindowVisible(hwnd):
+            print(hex(hwnd), win32gui.GetWindowText(hwnd))
+    win32gui.EnumWindows(winEnumHandler, None)
